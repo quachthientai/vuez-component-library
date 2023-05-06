10ca94617eec12996c01a05db9c20a57ae30e78a
Tai <quachthientai@gmail.com>
2023-05-06 17:07:20 -0500

    refactor scss rule for button component

--- src/App.vue
+++ src/App.vue
@@ -14,27 +14,124 @@
 </script>
 
 <template>
-  <div class="mt-3">
-    <Button btnClass="btn rounded btn-primary" :isLoading="false" text="Primary" iconPosition="right" icon="ph:envelope-bold"></Button>
-    <Button btnClass="btn btn-icon-circle btn-danger" icon="ph:envelope-bold"></Button>
-    <Button btnClass="btn btn-icon-circle btn-primary" icon="ph:envelope-bold"></Button>
-    <Button btnClass="btn btn-icon-circle btn-secondary" icon="ph:envelope-bold"></Button>
-    <Button btnClass="btn btn-icon-circle btn-warning" icon="ph:envelope-bold"></Button>
-    <Button btnClass="btn btn-secondary" :isLoading="false" text="Secondary" iconPosition="left" icon="ph:envelope-bold"></Button>
-    <Button btnClass="btn btn-danger" :isLoading="false" text="Danger" iconPosition="left" icon="ph:envelope-bold"></Button>
-    <Button btnClass="btn btn-warning" :isLoading="false" text="Warning" iconPosition="left" icon="ph:envelope-bold"></Button>
-    <Button btnClass="btn btn-info" :isLoading="false" text="Info" iconPosition="left" icon="ph:envelope-bold"></Button>
-    <Button btnClass="btn btn-success" :isLoading="false" text="Success" iconPosition="left" icon="ph:envelope-bold"></Button>
+  <div class="flex justify-start mt-2">
+    <Button btnClass="btn btn-primary" class="mr-2" text="Primary" iconPosition="right" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-secondary" class="mr-2" text="Secondary" iconPosition="left" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-danger" class="mr-2" text="Danger" iconPosition="left" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-warning" class="mr-2" text="Warning" iconPosition="left" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-info" class="mr-2" text="Info" iconPosition="left" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-success" class="mr-2" text="Success" iconPosition="left" icon="ph:envelope-bold"></Button>
   </div>
 
-  <div class="mt-3">
-    <Button btnClass="btn btn-outline-primary" :isLoading="true" text="Primary" iconPosition="right" icon="ph:envelope-bold"></Button>
-    <Button btnClass="btn btn-outline-secondary" :isLoading="true" text="Secondary" iconPosition="left" icon="ph:envelope-bold"></Button>
-    <Button btnClass="btn btn-outline-danger" :isLoading="true" text="Danger" iconPosition="left" icon="ph:envelope-bold"></Button>
-    <Button btnClass="btn btn-outline-warning" :isLoading="true" text="Warning" iconPosition="left" icon="ph:envelope-bold"></Button>
-    <Button btnClass="btn btn-outline-info" :isLoading="true" text="Info" iconPosition="left" icon="ph:envelope-bold"></Button>
-    <Button btnClass="btn btn-outline-success" :isLoading="true" text="Success" iconPosition="left" icon="ph:envelope-bold"></Button>
+  <div class="flex justify-start mt-2">
+    <Button btnClass="btn btn-primary" :isLoading="true" class="mr-2" text="Primary" iconPosition="right" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-secondary" :isLoading="true"  class="mr-2" text="Secondary" iconPosition="left" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-danger" :isLoading="true"  class="mr-2" text="Danger" iconPosition="left" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-warning" :isLoading="true"  class="mr-2" text="Warning" iconPosition="left" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-info" :isLoading="true"  class="mr-2" text="Info" iconPosition="left" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-success" :isLoading="true"  class="mr-2" text="Success" iconPosition="left" icon="ph:envelope-bold"></Button>
   </div>
+
+  <div class="flex justify-start mt-2">
+    <Button btnClass="btn rounded btn-primary" class="mr-2" text="Primary" iconPosition="right" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn rounded btn-secondary" class="mr-2" text="Secondary" iconPosition="left" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn rounded btn-danger" class="mr-2" text="Danger" iconPosition="left" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn rounded btn-warning" class="mr-2" text="Warning" iconPosition="left" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn rounded btn-info" class="mr-2" text="Info" iconPosition="left" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn rounded btn-success" class="mr-2" text="Success" iconPosition="left" icon="ph:envelope-bold"></Button>
+  </div>
+
+  <div class="flex justify-start mt-2">
+    <Button btnClass="btn btn-primary" :isDisabled="true" class="mr-2" text="Primary" iconPosition="right" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-secondary" :isDisabled="true" class="mr-2" text="Secondary" iconPosition="left" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-danger" :isDisabled="true" class="mr-2" text="Danger" iconPosition="left" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-warning" :isDisabled="true" class="mr-2" text="Warning" iconPosition="left" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-info" :isDisabled="true" class="mr-2" text="Info" iconPosition="left" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-success" :isDisabled="true" class="mr-2" text="Success" iconPosition="left" icon="ph:envelope-bold"></Button>
+  </div>
+
+  <div class="flex justify-start mt-2">
+    <Button btnClass="btn btn-outline-primary" class="mr-2" text="Primary" iconPosition="right" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-outline-secondary" class="mr-2" text="Secondary" iconPosition="left" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-outline-danger" class="mr-2" text="Danger" iconPosition="left" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-outline-warning" class="mr-2" text="Warning" iconPosition="left" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-outline-info" class="mr-2" text="Info" iconPosition="left" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-outline-success" class="mr-2" text="Success" iconPosition="left" icon="ph:envelope-bold"></Button>
+  </div>
+
+  <div class="flex justify-start mt-2">
+    <Button btnClass="btn btn-outline-primary" :isLoading="true" class="mr-2" text="Primary" iconPosition="right" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-outline-secondary" :isLoading="true"  class="mr-2" text="Secondary" iconPosition="left" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-outline-danger" :isLoading="true"  class="mr-2" text="Danger" iconPosition="left" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-outline-warning" :isLoading="true"  class="mr-2" text="Warning" iconPosition="left" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-outline-info" :isLoading="true"  class="mr-2" text="Info" iconPosition="left" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-outline-success" :isLoading="true"  class="mr-2" text="Success" iconPosition="left" icon="ph:envelope-bold"></Button>
+  </div>
+
+  <div class="flex justify-start mt-2">
+    <Button btnClass="btn rounded btn-outline-primary" class="mr-2" text="Primary" iconPosition="right" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn rounded btn-outline-secondary" class="mr-2" text="Secondary" iconPosition="left" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn rounded btn-outline-danger" class="mr-2" text="Danger" iconPosition="left" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn rounded btn-outline-warning" class="mr-2" text="Warning" iconPosition="left" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn rounded btn-outline-info" class="mr-2" text="Info" iconPosition="left" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn rounded btn-outline-success" class="mr-2" text="Success" iconPosition="left" icon="ph:envelope-bold"></Button>
+  </div>
+
+  <div class="flex justify-start mt-2">
+    <Button btnClass="btn btn-outline-primary" :isDisabled="true" class="mr-2" text="Primary" iconPosition="right" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-outline-secondary" :isDisabled="true" class="mr-2" text="Secondary" iconPosition="left" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-outline-danger" :isDisabled="true" class="mr-2" text="Danger" iconPosition="left" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-outline-warning" :isDisabled="true" class="mr-2" text="Warning" iconPosition="left" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-outline-info" :isDisabled="true" class="mr-2" text="Info" iconPosition="left" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-outline-success" :isDisabled="true" class="mr-2" text="Success" iconPosition="left" icon="ph:envelope-bold"></Button>
+  </div>
+
+  <div class="flex justify-start mt-2">
+    <Button btnClass="btn rounded btn-primary" class="mr-2" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn rounded btn-secondary" class="mr-2" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn rounded btn-danger" class="mr-2" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn rounded btn-warning" class="mr-2" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn rounded btn-info" class="mr-2" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn rounded btn-success" class="mr-2" icon="ph:envelope-bold"></Button>
+  </div>
+
+  <div class="flex justify-start mt-2">
+    <Button btnClass="btn rounded btn-outline-primary" class="mr-2" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn rounded btn-outline-secondary" class="mr-2" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn rounded btn-outline-danger" class="mr-2" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn rounded btn-outline-warning" class="mr-2" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn rounded btn-outline-info" class="mr-2" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn rounded btn-outline-success" class="mr-2" icon="ph:envelope-bold"></Button>
+  </div>
+
+  <div class="flex justify-start mt-2">
+    <Button btnClass="btn btn-icon-circle btn-primary" class="mr-2" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-icon-circle btn-secondary" class="mr-2" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-icon-circle btn-danger" class="mr-2" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-icon-circle btn-warning" class="mr-2" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-icon-circle btn-info" class="mr-2" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-icon-circle btn-success" class="mr-2" icon="ph:envelope-bold"></Button>
+  </div>
+
+  <div class="flex justify-start mt-2">
+    <Button btnClass="btn btn-icon-circle btn-outline-primary" class="mr-2" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-icon-circle btn-outline-secondary" class="mr-2" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-icon-circle btn-outline-danger" class="mr-2" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-icon-circle btn-outline-warning" class="mr-2" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-icon-circle btn-outline-info" class="mr-2" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-icon-circle btn-outline-success" class="mr-2" icon="ph:envelope-bold"></Button>
+  </div>
+
+  <div class="flex justify-start mt-2">
+    <Button btnClass="btn btn-sm btn-info" class="mr-2" text="Small" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-info" class="mr-2" text="regular" icon="ph:envelope-bold"></Button>
+    <Button btnClass="btn btn-lg btn-info" text="large" class="mr-2" icon="ph:envelope-bold"></Button>
+    
+  </div>
+  
+  
+
+  
   
 
 </template>
