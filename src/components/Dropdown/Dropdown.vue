5df0cb82e2e9cba6e921520e5aecfac8e6d84ffc
Tai <quachthientai@gmail.com>
2023-05-15 22:25:44 -0500

    Dropdown component init

---
+++ src/components/Dropdown/Dropdown.vue
@@ -0,0 +1,30 @@
+<script>
+   import Button from '../Button/Button.vue';
+   export default {
+      name: 'Dropdown',
+      components: {
+         Button
+      },
+      props: {
+         text: {
+            type: String,
+            default: null,
+         },
+         dropDownClass: {
+            type: String,
+            default: null,
+            required: true,
+         }
+   
+         
+      }
+   }
+</script>
+
+<template>
+   <Button :text="text" :btnClass="dropDownClass">
+      
+   </Button>
+</template>
+
+<style lang="scss"></style>
