fb6b9e56a6792ee113884bbb4a262b1a45a964d2
Tai <quachthientai@gmail.com>
2023-05-21 01:38:28 -0500

    update latest changed

--- src/components/Forms/Switch.vue
+++ src/components/Forms/Switch.vue
@@ -23,10 +23,6 @@
                 type: String,
                 default: null,
             },
-            isDisabled: {
-                type: Boolean,
-                default: false
-            },
             onStateIcon: {
                 type: String,
                 default: null,   
@@ -41,6 +37,14 @@
 
 <template>
     <label :class="switchClass">
+        <input 
+            checked
+            class="switch__input" 
+            @change="onChange" 
+            :disabled="isDisabled" type="checkbox"
+        >
+        <span class="switch__slider"></span>
+
         <template v-if="!(onStateIcon || offStateIcon) && (onStateText || offStateText)">
             <span class="switch__labels" :data-on="onStateText" :data-off="offStateText"></span>
         </template>
