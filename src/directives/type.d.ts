7de517cf7be59d85257bc96801dde3eb9b09e158
Tai <quachthientai@gmail.com>
2023-07-27 23:33:58 -0500

    drag n drop implemented

--- src/directives/type.d.ts
+++ src/directives/type.d.ts
@@ -1,11 +1,20 @@
-export type Event = MouseEvent | PointerEvent
+export type Event = MouseEvent | PointerEvent | DragEvent 
 
 export interface Binding {
    value?: any,
    oldValue?: any,
    arg?: any,
 }
+
 export type HandleRippleFunc = (event: Event, element: HTMLElement) => void;
 
 export type HandleClickOutFunc = (event: Event, element: HTMLElement, binding: Binding) => void;
 
+
+
+
+export type HandleDrag = (event: Event, element: HTMLElement) => void;
+
+export type HandleDrop = (event: Event, element: HTMLElement) => void;
+
+export type HandleDragOver = (event: Event) => void;
--- src/views/CardView.vue
+++ src/views/CardView.vue
@@ -15,7 +15,11 @@ export default {
 
 <template>
   <div class="m-5 w-[400px]">
-    <Card :elevation="1" outlined="false" title="This is titlaaae"> 
+    Pass as props
+    <Card :elevation="1" class="mb-5" :outlined="false" title="This is title (props)" subtitle="This is subtitle (props)"></Card>
+
+    Using slots
+    <Card :elevation="2" :outlined="false" title="This is titlaaae"> 
       <template v-slot:title>This is title</template>
       <template v-slot:sub-title>This is subtitle</template>
       <template v-slot:content>This is content</template>
