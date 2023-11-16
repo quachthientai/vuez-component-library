d3440cfb81f13798da1b167149204012c606d1c5
n2ai <longhai2511@gmail.com>
2023-11-15 23:36:04 -0600

    Working on input styling

--- src/components/Forms/Input/InputVue.tsx
+++ src/components/Forms/Input/InputVue.tsx
@@ -1,15 +1,16 @@
 import {computed, defineComponent} from "vue"
 
 const InputVue = defineComponent({
-    name: "Input",
-    props:{},
+    name: "InputVue",
+    
     setup(props,{slots,attrs}){
         return ()=>{
             return(
                 <>
-                    <div>
-                        Hello
-                    </div>
+                    <label class="relative">
+                        <input class="vz-input" ></input>
+                        <span class="absolute left-0 top-[0.1rem] mx-2 translate-y-4 transition duration-200">Username</span>
+                    </label>
                 </>
             )
         }
--- src/views/InputView.vue
+++ src/views/InputView.vue
@@ -1,15 +1,18 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
-import {InputVue} from '@/components/Forms/InputVue';
-export default defineComponent({
-    setup() {
-        
-    },
-})
+import { InputVue } from '@/components/Forms/Input/InputVue';
+
+export default {
+    name: 'InputView',
+    components: {
+        InputVue
+    }
+}
 </script>
 
 
 <template>
+    
     <p>Text Input</p>
     <input v-validate="{statement:'Validation Format of Text',minLength:3, maxLength:100}" class="input_test border border-slate-800" name="email"  type="text" />
     <p>Email Input</p>
@@ -17,10 +20,10 @@ export default defineComponent({
     <p>Date Input</p>
     <input v-validate="{statement:'Validation Format of Date', minDate:'10-20-2023', maxDate:'11-25-2023'}" class="input_test border border-slate-800" type="date">
     <br>
+    <br>
     <InputVue></InputVue>
-    
 </template>
 
 <style lang="scss">
     
-</style>@/components/Forms/InputVue
+</style>
