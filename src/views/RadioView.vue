81ad69c4ce376191ffba94efbc72009c77d8770d
Tai <quachthientai@gmail.com>
2023-12-28 08:33:32 -0600

    refactoring radio component

--- src/views/RadioView.vue
+++ src/views/RadioView.vue
@@ -1,23 +1,21 @@
 <script lang="ts">
-
+import { Radio } from '@/components/Radio/Radio';
 
 export default {
   name: 'RadioView',
   components: {
-
+    Radio
   }
 }
 </script>
 
 <template>
-  <div>This is home view</div>
-
-  <!-- <Radio radioClass="radio radio-primary" radioValue="success"></Radio>
-  <Radio radioClass="radio radio-secondary" radioValue="secondary"></Radio>
-  <Radio radioClass="radio radio-info" radioValue="info"></Radio>
-  <Radio radioClass="radio radio-warning" radioValue="warning"></Radio>
-  <Radio radioClass="radio radio-danger" radioValue="danger"></Radio>
-  <Radio radioClass="radio radio-success" radioValue="success"></Radio> -->
+  <div class="m-[200px]">
+    <Radio value="2" label="Option 2" color="info"></Radio>
+  </div>
+  
+  
+  
   
   
 
